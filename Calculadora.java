3d0665fb43481f0ab3ab13ef4ce1f30acf841681
Eliane Regina Nicácio Mendes <elianemendes@Elianes-MacBook-Air.local>
2021-12-25 01:51:33 -0300

    fix: constante emissão de carbono

--- Calculadora.java
+++ Calculadora.java
@@ -5,8 +5,8 @@ public class Calculadora {
 
     public static void main(String[] args) {
         double COTACAODOLAR = 5.67;
-        double EMISSAOCARBONOGASOLINA = 2.2755;
-        double EMISSAOCARBONOETANOL = (EMISSAOCARBONOGASOLINA * 0.73);
+        double EMISSAOCARBONOGASOLINA = 2.264; // MACEDO et al. (2004)
+        double EMISSAOCARBONOETANOL = 1.722; // MACEDO et al. (2004)
         double PRECOCARBONOTON = 3.31;
         // Fórmula de cálculo de emissão de carbono para Gasolina -> 1 litro de gasolina
         // = 1 x 0,82 x 0,75 x 3,7 = total de kg CO2 emitido por litro.
@@ -72,8 +72,49 @@ public class Calculadora {
 
         // *********** SAÍDAS ************//
 
-        System.out.printf("O seu custo mensal com o abastecimendo por %s é de %.2f", combustivel, custoFinal);
-        System.out.printf("O custo real por litro de %s é de %.2f", combustivel, custoFinalPorLitro);
+        System.out.printf("O seu custo mensal com o abastecimendo por %s é de R$%.2f\n", combustivel, custoFinal);
+        System.out.printf("O custo real por litro de %s é de R$%.2f", combustivel, custoFinalPorLitro);
+
+
+        // Testar para o outro combustível
+        System.out.println("Gostaria de saber os custos para abastecimento com o outro combustível?");
+        System.out.println("S - Sim / N - Não");
+        var outroCombustivel = entrada.nextLine();
+
+        if (outroCombustivel.equalsIgnoreCase("S")) {
+            System.out.println("Qual o valor do outro combustível?");
+            double precoOutro = entrada.nextDouble();
+
+            double gastoMensalOutro = consumoMensalLitro * precoOutro;
+
+            if (combustivel.equalsIgnoreCase("G")) {
+                carbonoEmitido = consumoMensalLitro * EMISSAOCARBONOETANOL;
+            } else {
+                carbonoEmitido = consumoMensalLitro * EMISSAOCARBONOGASOLINA;
+            }
+
+            // Custo da emissão
+            double custoDaEmissaoOutro = carbonoEmitido * custoCarbonoReal;
+
+            // Custos reais
+            double custoFinalOutro = gastoMensalOutro + custoDaEmissaoOutro;
+
+            // Custo por litro Outro
+            double custoFinalPorLitroOutro = 0;
+            if (combustivel.equalsIgnoreCase("G")) {
+                custoFinalPorLitroOutro = precoOutro + (EMISSAOCARBONOETANOL * custoCarbonoReal);
+            } else {
+                custoFinalPorLitroOutro = precoOutro + (EMISSAOCARBONOGASOLINA * custoCarbonoReal);
+            }
+
+            // *********** SAÍDAS ************//
+            
+            System.out.printf("O custo mensal com o abastecimendo por este combustível seria de R$%.2f\n", combustivel,
+                    custoFinalOutro);
+            System.out.printf("E custo real por litro seria de R$%.2f", combustivel, custoFinalPorLitroOutro);
+        } else {
+            System.out.println("Calculadora Encerrada");
+        }
 
         entrada.close();
     }
